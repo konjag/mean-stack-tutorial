217e2651fbc8dfcf995905db8bfe8011dd40df02
konjag <konrad.jaguszewski@gmail.com>
2019-06-10 13:05:02 +0200

    Remove unnecessary code

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -1,7 +1,5 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user.model');
 
 passport.use(new LocalStrategy({
